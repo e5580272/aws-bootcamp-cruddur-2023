7038f6230fd1129896d8cc57496d88e27596f6e3
e5580272 Leary <kazuka96@msn.com>
2024-03-24 22:28:10 +0000

    syntax error

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -19,7 +19,7 @@ from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import Tracerprovider
+from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
